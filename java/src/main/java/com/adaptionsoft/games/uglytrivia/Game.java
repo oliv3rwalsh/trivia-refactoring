20a9d865a12d272a3fba6345e4ff8277f40660fc
oliv3rwalsh <oliverwalsh45@gmail.com>
2022-09-23 12:14:25 -0500

    refactored currentCategory function

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -100,15 +100,15 @@ public class Game {
 	
 	
 	private String currentCategory() {
-		if (places[currentPlayer] == 0) return "Pop";
-		if (places[currentPlayer] == 4) return "Pop";
-		if (places[currentPlayer] == 8) return "Pop";
-		if (places[currentPlayer] == 1) return "Science";
-		if (places[currentPlayer] == 5) return "Science";
-		if (places[currentPlayer] == 9) return "Science";
-		if (places[currentPlayer] == 2) return "Sports";
-		if (places[currentPlayer] == 6) return "Sports";
-		if (places[currentPlayer] == 10) return "Sports";
+		if(places[currentPlayer] == 0 || places[currentPlayer] % 4 == 0){
+			return "Pop";
+		}
+		if(places[currentPlayer] == 1 || places[currentPlayer]-1 % 4 == 0){
+			return "Science";
+		}
+		if(places[currentPlayer] == 2 || places[currentPlayer]-2 % 4 == 0){
+			return "Sports";
+		}
 		return "Rock";
 	}
 
